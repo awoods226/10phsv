1a9ee9e477197ac1cd5a86629c1ea418acd280a4
Ari Woods <awoods226@gmail.com>
2018-07-12 21:31:46 -0500

    progress on schedule

--- src/components/Schedule/Schedule.tsx
+++ src/components/Schedule/Schedule.tsx
@@ -1,6 +1,7 @@
 import * as React from "react";
 import * as Calendar from "react-big-calendar";
 import "react-big-calendar/lib/css/react-big-calendar.css";
+import "./schedule.css";
 import * as moment from "moment";
 Calendar.setLocalizer(Calendar.momentLocalizer(moment));
 
@@ -16,24 +17,72 @@ export default class Schedule extends React.Component<
   constructor(props: ScheduleProps) {
     super(props);
     this.state = {
-      events: [
-        {
-          title: "All Levels BJJ",
-          start: new Date(new Date().setHours(new Date().getHours() - 3)),
-          end: new Date(new Date().setHours(new Date().getHours() + 3))
-        },
-        {
-          title: "BJJ Fundamentals",
-          start: new Date(new Date().setHours(new Date().getHours() - 3)),
-          end: new Date(new Date().setHours(new Date().getHours() + 3))
-        },
-        {
-          title: "Open Mat",
-          start: new Date(new Date().setHours(new Date().getHours() - 3)),
-          end: new Date(new Date().setHours(new Date().getHours() + 3))
-        }
-      ]
+      events: []
     };
+  }
+  getEventSchedule = (
+    title: string,
+    days: string[],
+    startHour: number,
+    startMin: number,
+    endHour: number,
+    endMin: number
+  ) => {
+    let daysWithTimes = days.map(v => {
+      return moment().day(v);
+    });
+    return daysWithTimes.map(v => {
+      return {
+        title: title,
+        start: v
+          .hours(startHour)
+          .minutes(startMin)
+          .toDate(),
+        end: v
+          .hours(endHour)
+          .minutes(endMin)
+          .toDate()
+      };
+    });
+  };
+  getFundamentalsEvents = () => {
+    return this.getEventSchedule(
+      "BJJ Fundamentals",
+      ["Tuesday", "Wednesday", "Thursday", "Friday"],
+      17,
+      0,
+      18,
+      30
+    );
+  };
+  getAllLevelsEvents = () => {
+    return this.getEventSchedule("All Levels BJJ", ["Monday"], 17, 30, 19, 0);
+  };
+  getEvents = () => {
+    let events = [];
+    let allLevels = this.getAllLevelsEvents();
+    events.push(...allLevels);
+    let fundamentals = this.getFundamentalsEvents();
+    events.push(...fundamentals);
+    let openMat = this.getOpenMatEvents();
+    events.push(...openMat);
+    let muayThai = this.getMuayThaiEvents();
+    events.push(...muayThai);
+    return events;
+  };
+  getOpenMatEvents = () => {
+    return this.getEventSchedule("Open Mat", ["Saturday"], 10, 0, 12, 0);
+  };
+  getMuayThaiEvents = () => {
+    return this.getEventSchedule("Muay Thai", ["Wednesday"], 16, 0, 17, 0);
+  };
+  componentDidMount() {
+    let schedule = this.getEvents();
+    let events = [];
+    schedule.map(s => {
+      events.push(s);
+    });
+    this.setState({ events: events });
   }
   public render() {
     return (
@@ -42,11 +91,13 @@ export default class Schedule extends React.Component<
           defaultDate={new Date()}
           defaultView="week"
           toolbar={false}
-          timeslots={6}
-          step={30}
+          step={60}
+          showMultiDayTimes
+          min={new Date(2017, 10, 0, 10, 0, 0)}
+          max={new Date(2017, 10, 0, 20, 0, 0)}
           events={this.state.events}
           selectable={false}
-          style={{ height: "100vh" }}
+          style={{ height: "40vh" }}
         />
       </div>
     );
